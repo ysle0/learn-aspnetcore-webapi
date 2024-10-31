23d1ea84a187a2b6b3154184c0084446405b3dcc
yoonsang lee <highp0912@pm.me>
2024-11-01 01:00:57 +0900

    fix: Stock record->class: EF migration

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -2,15 +2,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models;
 
-public record Stock(
-  int Id,
-  string CompanyName,
-  string Symbol,
-  [property: Column(TypeName = "decimal(18, 2)")]
-  decimal Purchase,
-  [property: Column(TypeName = "decimal(18, 2)")]
-  decimal LastDividend,
-  string Industry,
-  long MarketCap,
-  List<Comment> Comments
-);
+public class Stock {
+  public int Id { get; init; }
+  public string CompanyName { get; init; }
+  public string Symbol { get; init; }
+
+  [Column(TypeName = "decimal(18, 2)")] public decimal Purchase { get; init; }
+
+  [Column(TypeName = "decimal(18, 2)")] public decimal LastDividend { get; init; }
+
+  public string Industry { get; init; }
+  public long MarketCap { get; init; }
+  public List<Comment> Comments { get; init; }
+}
